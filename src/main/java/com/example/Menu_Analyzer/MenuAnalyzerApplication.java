9110b5ae1314c1923e6cad90aa3401b45db7e94b
Loveesh Singh <loveesh.20132@gmail.com>
2026-02-25 13:46:43 +0530

    Initial Menu Analyzer backend setup

---
+++ src/main/java/com/example/Menu_Analyzer/MenuAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Menu_Analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuAnalyzerApplication.class, args);
+	}
+
+}
